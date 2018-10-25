af55c88ee3f23178107c5e656b71bdd4379bd3cf
Ilya khvesevich <rairetdev@gmail.com>
2018-10-25 18:18:26 +0300

    bamboo header block: desktop version

---
+++ src/js/animations/_bamboo-anim.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  var line = $('.js-anim-line-scale');
+  var circle = $('.js-anim-circle-scale');
+  var circleLg = $('.js-anim-circle-lg-scale');
+  var halfCircle = $('.js-anim-rotate');
+
+
+  halfCircle.addClass('bamboo-header__bg_half-circle-full');
+  circle.addClass('bamboo-header__bg_circle-full');
+  circleLg.addClass('bamboo-header__bg_circle-lg-full');
+
+
+  window.setInterval(function() {
+    line.toggleClass('bamboo-header__bg_line-full');
+  }, 1500);
+
+
+});
--- src/js/animations/index.js
+++ src/js/animations/index.js
@@ -1,3 +1,4 @@
 import './_dust';
 import './_gravity';
 import './_slidesAnimation';
+import './_bamboo-anim';
